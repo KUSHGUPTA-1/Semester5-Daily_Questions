6edb270043df34ad7783ea5824a6b5d4eeaec158
KUSH GUPTA <2020kushgupta@gmail.com>
2025-08-22 20:01:58 +0530

    Add files via upload

---
+++ Minimum_Area_To_Cover_All_Ones.java
@@ -0,0 +1,23 @@
+public class Minimum_Area_To_Cover_All_Ones {
+    public int minimumArea(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int sr=n-1;
+        int er=0;
+        int sc=m-1;
+        int ec=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    sr=Math.min(sr,i);
+                    er=Math.max(er,i);
+                    sc=Math.min(sc,j);
+                    ec=Math.max(ec,j);
+                }
+            }
+        }
+        int height=er-sr+1;
+        int breadth=ec-sc+1;
+        return height*breadth;
+    }
+}
