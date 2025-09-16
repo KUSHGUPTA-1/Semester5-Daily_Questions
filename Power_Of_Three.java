daa6442e0ccf155991f7b3711bfbfaed204f4b1e
KUSH GUPTA <2020kushgupta@gmail.com>
2025-09-16 18:01:27 +0530

    Add files via upload

---
+++ Power_Of_Three.java
@@ -0,0 +1,13 @@
+public class Power_Of_Three {
+    public boolean isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        else if(n==0){
+            return false;
+        }
+        else{
+            return n%3==0 && isPowerOfThree(n/3);
+        }
+    }
+}
