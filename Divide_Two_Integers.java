70b87d3457ccce2eafaa4844aa06e1b4a42c69f2
KUSH GUPTA <2020kushgupta@gmail.com>
2025-09-25 16:54:39 +0530

    Add files via upload

---
+++ Divide_Two_Integers.java
@@ -0,0 +1,10 @@
+public class Divide_Two_Integers {
+    public int divide(int dividend, int divisor) {
+        if(dividend==-2147483648 && divisor==-1){
+            return 2147483647;
+        }
+        else{
+            return dividend/divisor;
+        }
+    }
+}
