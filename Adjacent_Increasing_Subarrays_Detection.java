4de41cda724b078b515f90a362a0d32cf3e6f412
KUSH GUPTA <2020kushgupta@gmail.com>
2025-10-14 20:01:46 +0530

    Add files via upload

---
+++ Adjacent_Increasing_Subarrays_Detection.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Adjacent_Increasing_Subarrays_Detection {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k){
+        int n=nums.size();
+        for(int i=0;i+2*k<=n;i++){
+            boolean first=true,second=true;
+            for(int j=i;j<i+k-1;j++){
+                if(nums.get(j)>=nums.get(j+1)){
+                    first=false;
+                    break;
+                }
+            }
+            for(int j=i+k;j<i+2*k-1;j++){
+                if(nums.get(j)>=nums.get(j+1)){
+                    second=false;
+                    break;
+                }
+            }
+            if(first && second){
+                return true;
+            }
+        }
+        return false;
+        // TC-> O(n*k).
+        // SC-> O(1).
+    }
+}
