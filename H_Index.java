af84c45a93cd59a25d375ff1fb67f3c9ab0a3003
KUSH GUPTA <2020kushgupta@gmail.com>
2025-12-14 23:48:36 +0530

    Add files via upload

---
+++ H_Index.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class H_Index {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int left=0;
+        int right=n-1;
+        Arrays.sort(citations);
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(citations[mid]==n-mid){
+                return n-mid;
+            }
+            else if(citations[mid]<n-mid){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return n-left;
+    }
+}
