1c2391554d58fc0428d67c46848a78f760a2015b
KUSH GUPTA <2020kushgupta@gmail.com>
2025-12-22 15:08:21 +0530

    Add files via upload

---
+++ Divisor_Game.java
@@ -0,0 +1,12 @@
+public class Divisor_Game {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        // Time Complexity: O(1)
+        // Space Complexity: O(1)
+    }
+}
