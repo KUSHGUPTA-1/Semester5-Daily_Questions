e35fc6a171f2a905c7b49bd3ffa388ac2f618bda
KUSH GUPTA <2020kushgupta@gmail.com>
2026-01-09 20:00:38 +0530

    Add files via upload

---
+++ Find_The_Maximum_Length_Of_Valid_Subsequence_2.java
@@ -0,0 +1,16 @@
+public class Find_The_Maximum_Length_Of_Valid_Subsequence_2 {
+    public int maximumLength(int[] nums, int k) {
+        int[][] dp=new int[k][k];
+        int res=0;
+        for(int num:nums){
+            num%=k;
+            for(int prev=0;prev<k;prev++){
+                dp[prev][num]=dp[num][prev]+1;
+                res=Math.max(res,dp[prev][num]);
+            }
+        }
+        return res;
+        //TC-> O(N*K)
+        //SC-> O(K*K)
+    }
+}
