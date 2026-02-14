61e220c3682a90cb2652cac4c6820c061f2a8a10
KUSH GUPTA <2020kushgupta@gmail.com>
2026-02-14 16:15:43 +0530

    Add files via upload

---
+++ Maximum_Width_Ramp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Maximum_Width_Ramp {
+    public int maxWidthRamp(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<nums.length; i++)
+            if(stack.isEmpty() || nums[i] < nums[stack.peek()])
+                stack.push(i);
+        int ans = 0;
+        for(int i=nums.length-1; i>=0; i--)
+            while(!stack.isEmpty() && nums[i] >= nums[stack.peek()])
+                ans = Math.max(ans, i - stack.pop());
+        return ans;
+        //TC:O(n)
+        //SC:O(n)
+    }
+}
