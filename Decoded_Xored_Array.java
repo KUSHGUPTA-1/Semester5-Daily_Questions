443eb30dc2f8574f15465b6ddd85519ddccbccc9
KUSH GUPTA <2020kushgupta@gmail.com>
2026-03-06 17:14:05 +0530

    Add files via upload

---
+++ Decoded_Xored_Array.java
@@ -0,0 +1,12 @@
+class Decoded_Xored_Array {
+    public int[] decode(int[] encoded, int first) {
+        int[] decoded = new int[encoded.length + 1];
+        decoded[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            decoded[i + 1] = decoded[i] ^ encoded[i];
+        }
+        return decoded;
+        //TC: O(n) where n is the length of the encoded array.
+        //SC: O(n) where n is the length of the decoded array.
+    }
+}
