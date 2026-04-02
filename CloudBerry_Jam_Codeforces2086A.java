a845a33df096ea5cccf74e54dbb7773cd1b6ce50
KUSH GUPTA <2020kushgupta@gmail.com>
2026-04-02 08:57:48 +0530

    Add files via upload

---
+++ CloudBerry_Jam_Codeforces2086A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CloudBerry_Jam_Codeforces2086A{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            System.out.println(2*n);
+        }
+        sc.close();
+        //TC: O(t) for t test cases, O(1) for each test
+        //SC: O(1) for storing the input and output, O(1) for other
+    }
+}
