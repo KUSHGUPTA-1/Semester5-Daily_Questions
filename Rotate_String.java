3b7c27c63c3af0d494a723f038ebb8d96a0d5001
KUSH GUPTA <2020kushgupta@gmail.com>
2026-05-04 20:47:02 +0530

    Add files via upload

---
+++ Rotate_String.java
@@ -0,0 +1,16 @@
+public class Rotate_String {
+    public boolean rotateString(String s, String goal){
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        String word=s+s;
+        for(int i=0;i<s.length();i++){
+            if(word.substring(i,i+s.length()).equals(goal)){
+                return true;
+            }
+        }
+        return false;
+        //TC-> O(n^2)
+        //SC-> O(n)
+    }
+}
