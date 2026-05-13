9dfd99788fa477f55c29031bc3558947860866f2
KUSH GUPTA <2020kushgupta@gmail.com>
2026-05-13 19:30:27 +0530

    Implement main method for Add_To_Array_Form_Of_Integer
    Added a main method to test the addToArrayForm function.

--- Add_To_Array_Form_Of_Integer.java
+++ Add_To_Array_Form_Of_Integer.java
@@ -1,18 +1,22 @@
 import java.util.*;
 public class Add_To_Array_Form_Of_Integer {
-    public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> l1=new ArrayList<>();
-        int i=num.length-1;
-        while(i>=0 || k>0){
-            if(i>=0){
-               k+=num[i];
-               i--; 
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> l1 = new ArrayList<>();
+        int i = num.length - 1;
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
             }
-            l1.add(0,k%10);
-            k/=10;
+            l1.add(0, k % 10);
+            k /= 10;
         }
         return l1;
-        //TC:O(max(n,log(k)))
-        //SC:O(max(n,log(k)))
+    }
+    public static void main(String[] args) {
+        int[] num = {1, 2, 0, 0};
+        int k = 34;
+        List<Integer> ans = addToArrayForm(num, k);
+        System.out.println(ans);
     }
 }
