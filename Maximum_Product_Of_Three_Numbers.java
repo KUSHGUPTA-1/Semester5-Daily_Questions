2831d69944c15c1689b0269ead2d6aafa7634e6e
KUSH GUPTA <2020kushgupta@gmail.com>
2026-07-26 20:35:15 +0530

    Add files via upload

---
+++ Maximum_Product_Of_Three_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Maximum_Product_Of_Three_Numbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int ans1=nums[n-1]*nums[n-2]*nums[n-3];
+        int ans2=nums[0]*nums[1]*nums[n-1];
+        return Math.max(ans1,ans2);
+        //TC->O(nlogn) 
+        // SC->O(1)
+    }
+}
