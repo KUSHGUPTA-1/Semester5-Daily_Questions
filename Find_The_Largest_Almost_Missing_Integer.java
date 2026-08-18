6a4d0709f0adeb8499b4097c48ea2a580ff42e52
KUSH GUPTA <2020kushgupta@gmail.com>
2026-08-18 21:31:05 +0530

    Add files via upload

---
+++ Find_The_Largest_Almost_Missing_Integer.java
@@ -0,0 +1,28 @@
+public class Find_The_Largest_Almost_Missing_Integer {
+    public int largestInteger(int[] nums, int k) {
+        int n=nums.length;
+        int freq[]=new int[51];
+        for(int num:nums){
+            freq[num]++;
+        }
+        if(k==1){
+            for(int i=50;i>=0;i--){
+                if(freq[i]==1){
+                    return i;
+                }
+            }
+            return -1;
+        }
+        if(k==n){
+            int ans=0;
+            for(int num:nums){
+                ans=Math.max(ans,num);
+            }
+            return ans;
+        }
+        int ans=-1;
+        if(freq[nums[0]]==1) ans=Math.max(ans,nums[0]);
+        if(freq[nums[n-1]]==1) ans=Math.max(ans,nums[n-1]);
+        return ans;
+    }
+}
