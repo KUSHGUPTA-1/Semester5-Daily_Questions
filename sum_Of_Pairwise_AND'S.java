8c3c9f103141fe95745b461fbbb010fa61553aa6
KUSH GUPTA <2020kushgupta@gmail.com>
2026-09-06 22:27:35 +0530

    Add files via upload

---
+++ sum_Of_Pairwise_AND'S.java
@@ -0,0 +1,17 @@
+class Sum_Of_Pairwise_ANDS {
+        public long pairAndSum(int[] arr) {
+        long ans = 0;
+        for (int bit = 0; bit < 31; bit++) {
+            long count = 0;
+            for (int x : arr) {
+                if ((x & (1 << bit)) != 0) {
+                    count++;
+                }
+            }
+            ans += count * (count - 1) / 2 * (1L << bit);
+        }
+        return ans;
+        //TC->O(31*N) 
+        //SC->O(1)
+    }
+}
